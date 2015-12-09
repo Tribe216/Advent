fe70fc46b3cadccab22ea8698ebcf3b5e2492246
Bart Mach <bart.mach@gmail.com>
2015-12-09 20:50:51 +0100

    day 8!

--- day_8.py
+++ day_8.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
+import json
 
+raw_length = 0
+enc_length = 0
+for line in open('day_8_input.txt','r').readlines():
+    line = line.strip()
+    print line, len(line)
+    enc_line = json.dumps(line)
+    
+    print enc_line,len(enc_line)
+    raw_length += len(line)
+    enc_length += len(enc_line)
+print enc_length - raw_length
